4b18002b0bd306900c90045511c4f2201e473226
Rob Mumford <rlmumford@googlemail.com>
2018-10-16 19:16:58 +0100

    by rlmumford: Class overrides.

---
+++ modules/util/flexilayout_builder/src/EventSubscriber/BlockComponentRenderArray.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\flexilayout_builder\EventSubscriber;
+
+use Drupal\block_content\Access\RefinableDependentAccessInterface;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Render\Element;
+use Drupal\Core\Render\PlaceholderInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\layout_builder\Access\LayoutPreviewAccessAllowed;
+use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
+use Drupal\layout_builder\LayoutBuilderEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Builds render arrays and handles access for all block components.
+ */
+class BlockComponentRenderArray implements EventSubscriberInterface {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Creates a BlockComponentRenderArray object.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   */
+  public function __construct(AccountInterface $current_user) {
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY] = ['onBuildRender', 0];
+    return $events;
+  }
+
+  /**
+   * Builds render arrays for block plugins and sets it on the event.
+   *
+   * @param \Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent $event
+   *   The section component render event.
+   */
+  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event) {
+    $block = $event->getPlugin();
+    if (!$block instanceof BlockPluginInterface) {
+      return;
+    }
+
+    $component = $event->getComponent();
+    $build = $event->getBuild();
+    if ($component->get('class')) {
+      foreach (explode(' ', $component->get('class')) as $class) {
+        $build['#attributes']['class'][] = $class;
+      }
+    }
+    $event->setBuild($build);
+  }
+
+}
