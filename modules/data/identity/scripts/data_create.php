fad7449095295d73bb985c9a3708f2c257f7f98a
Rob Mumford <rlmumford@googlemail.com>
2020-11-18 15:57:22 +0000

    by rlmumford: Fix script

--- modules/data/identity/scripts/data_create.php
+++ modules/data/identity/scripts/data_create.php
@@ -5,18 +5,18 @@ use Drupal\identity\IdentityDataGroup;
 $identity_data_storage = \Drupal::entityTypeManager()->getStorage('identity_data');
 
 $phone_number = $identity_data_storage->create([
-'type' => 'telephone_number',
-'telephone_number' => '02089493303',
-'can_sms' => TRUE,
+  'class' => 'telephone_number',
+  'telephone_number' => '02089423236',
+  'can_sms' => TRUE,
 ]);
 $name = $identity_data_storage->create([
-'type' => 'personal_name',
-'full_name' => 'James Everall',
-'name' => [
-'given' => 'James',
-'family' => 'Everall',
-],
-'name_type' => 'full',
+  'class' => 'personal_name',
+  'full_name' => 'Jon Emerson',
+  'name' => [
+    'given' => 'Jon',
+    'family' => 'Emerson',
+  ],
+  'name_type' => 'full',
 ]);
 
 $group = new IdentityDataGroup([$name, $phone_number]);
