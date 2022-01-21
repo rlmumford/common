dffaef77d67f02b397f0816b2fc6d4888c594f88
Rob Mumford <rlmumford@googlemail.com>
2022-01-21 15:19:43 +0000

    by rlm: Fix bug with allowed values functions where entity is not yet available.

--- modules/data/identity/src/Entity/IdentityData.php
+++ modules/data/identity/src/Entity/IdentityData.php
@@ -301,12 +301,16 @@ class IdentityData extends ContentEntityBase implements IdentityDataInterface, E
    * Get the allowed values for the type field
    *
    * @param \Drupal\Core\Field\FieldStorageDefinitionInterface $definition
-   * @param \Drupal\identity\Entity\IdentityData $entity
+   * @param \Drupal\identity\Entity\IdentityData|NULL  $entity
    * @param $cacheable
    *
    * @return array
    */
-  public static function typeAllowedValues(FieldStorageDefinitionInterface $definition, IdentityData $entity, &$cacheable) {
+  public static function typeAllowedValues(FieldStorageDefinitionInterface $definition, IdentityData $entity = NUll, &$cacheable = TRUE) {
+    if (!$entity) {
+      return [];
+    }
+
     $cacheable = FALSE;
     return $entity->getClass()->typeOptions();
   }
