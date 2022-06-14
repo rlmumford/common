4dd825815d2d8ee07ba1837c9176a3fb6b1e6fb7
Rob Mumford <rlmumford@googlemail.com>
2022-06-14 02:56:10 +0100

    by rlm: Update PHPCS command and fix introduced errors.

--- modules/data/checklist/src/Ajax/EnsureItemInProgressCommand.php
+++ modules/data/checklist/src/Ajax/EnsureItemInProgressCommand.php
@@ -3,10 +3,13 @@
 namespace Drupal\checklist\Ajax;
 
 /**
- *
+ * Command to ensure an item is in progress.
  */
 class EnsureItemInProgressCommand extends ChecklistItemCommand {
 
+  /**
+   * {@inheritdoc}
+   */
   protected $command = 'itemEnsureInProgress';
 
 }
